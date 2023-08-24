b46cf4bc00aaf19579aa2018ccb8055b37db7ea4
Jonatan <jonatan.vargasportillo@gmail.com>
2023-08-24 18:38:08 -0300

    clase UnEmpleado: se utiliza scanner para ingresar los datos

--- Practico2/UnEmpleado.java
+++ Practico2/UnEmpleado.java
@@ -1,9 +1,37 @@
+import java.util.Scanner;
 
 public class UnEmpleado
 {
     public static void main(String[] args){
-        Empleado empleado = new Empleado(41442040,"Vargas Portillo","Jonatan",30000,2001);
-        empleado.mostrar();
-        empleado.mostrarLinea();
+        Scanner teclado = new Scanner(System.in);
+        int pregunta = 0;
+        do {
+            System.out.println("Ingrese un cuit: ");
+            long cuit = teclado.nextLong();
+
+            // Limpiar el búfer del scanner después de nextLong()
+            teclado.nextLine();
+
+            System.out.println("Ingrese su apellido: ");
+            String apellido = teclado.nextLine();
+
+            System.out.println("Ingrese su nombre: ");
+            String nombre = teclado.nextLine();
+
+            System.out.println("Ingrese el sueldo basico: ");
+            double sueldo = teclado.nextDouble();
+
+            System.out.println("Ingrese el anio de ingreso: ");
+            int anioIngreso = teclado.nextInt();
+
+            Empleado empleado = new Empleado(cuit, apellido, nombre, sueldo, anioIngreso);
+            empleado.mostrar();
+            empleado.mostrarLinea();
+
+            System.out.println("Desea ingresar otro empleado ? 1-Si 0-No");
+            pregunta = teclado.nextInt();
+        } while (pregunta != 0);
+
+        teclado.close();
     }
 }
