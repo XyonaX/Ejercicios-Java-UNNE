fee6f89f65855e98090e4b524e6cfdb9c0d6e3dd
Jonatan <jonatan.vargasportillo@gmail.com>
2023-08-28 17:56:40 -0300

    se modificaron los mains de los ejercicios 3 y 4

--- Practico2/UnAlumno.java
+++ Practico2/UnAlumno.java
@@ -4,14 +4,15 @@ public class UnAlumno
 
     public static void main(String[] args)
     {
-        Alumno alumno1 = new Alumno(1,"Jonatan","Vargas");
-        Alumno alumno2 = new Alumno(2," Juan","Perez");
+        int id = Integer.parseInt(args[0]);
+        String nombre = args[1];
+        String apellido = args[2];
+
+        
+        Alumno alumno1 = new Alumno(id, nombre, apellido);
         alumno1.setNota1(10);
         alumno1.setNota2(9);
-        alumno2.setNota1(3);
-        alumno2.setNota2(5);
         
         alumno1.mostrar();
-        alumno2.mostrar();
     }
 }
--- Practico2/UnCliente.java
+++ Practico2/UnCliente.java
@@ -2,10 +2,18 @@
 public class UnCliente
 {
   public static void main(String[] args){
-      Cliente unCliente = new Cliente(41442040,"Vargas","Jonatan",0.0);
-      unCliente.agregaSaldo(5000);
-      unCliente.mostrar();
-      unCliente.nuevoSaldo(0);
-      unCliente.mostrar();
+     int dni = Integer.parseInt(args[0]);
+     String apellido = args[1];
+     String nombre = args[2];
+     double saldoInicial = Double.parseDouble(args[3]);
+
+
+     Cliente unCliente = new Cliente(dni, apellido, nombre, saldoInicial);
+
+
+     unCliente.agregaSaldo(5000);
+     unCliente.mostrar();
+     unCliente.nuevoSaldo(0);
+     unCliente.mostrar();
   }
 }
