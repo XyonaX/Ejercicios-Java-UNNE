982dd332361e734200d42fd3e77677eba20e9f6e
XyonaX <jonatan.vargasportillo@gmail.com>
2023-09-19 02:26:59 -0300

    se realizo cambios en Banco y AplicacionBanco para adaptarse al ejercicio 4

--- Practico4/AplicacionBanco.java
+++ Practico4/AplicacionBanco.java
@@ -7,21 +7,36 @@ public class AplicacionBanco {
         fechaHoy.set(2010, 1, 12);
         Localidad localidadBanco = new Localidad("Corrientes", "Corrientes");
         ArrayList<Empleado> empleados = new ArrayList<>();
-        Empleado juan = new Empleado(20123310221L, "Perez", "Juan", 200000, fechaHoy);
-        Empleado ezequiel = new Empleado(14166574281L, "Rodriguez", "Ezequiel", 352400, fechaHoy);
-        Empleado nicolas = new Empleado(12023145221L, "Sanchez", "Nicolas", 550000, fechaHoy);
+        // Empleado juan = new Empleado(20123310221L, "Perez", "Juan", 200000,
+        // fechaHoy);
+        // Empleado ezequiel = new Empleado(14166574281L, "Rodriguez", "Ezequiel",
+        // 352400, fechaHoy);
+        // Empleado nicolas = new Empleado(12023145221L, "Sanchez", "Nicolas", 550000,
+        // fechaHoy);
 
-        Banco provincia = new Banco("Provincia", localidadBanco, 1, empleados);
+        // Banco provincia = new Banco("Provincia", localidadBanco, 1, empleados);
 
-        provincia.agregarEmpleado(juan);
-        provincia.agregarEmpleado(ezequiel);
-        provincia.agregarEmpleado(nicolas);
-        provincia.mostrar();
+        // Se probo el ejercicio3
+        // provincia.agregarEmpleado(juan);
+        // provincia.agregarEmpleado(ezequiel);
+        // provincia.agregarEmpleado(nicolas);
+        // provincia.mostrar();
 
-        provincia.quitarEmpleado(nicolas);
-        provincia.quitarEmpleado(juan);
-        provincia.quitarEmpleado(ezequiel);
-        provincia.mostrar();
+        // provincia.quitarEmpleado(nicolas);
+        // provincia.quitarEmpleado(juan);
+        // provincia.quitarEmpleado(ezequiel);
+        // provincia.mostrar();
 
+        // Vamos a probar lo que seria el ejercicio 4
+
+        ArrayList<CuentaBancaria> cuentas = new ArrayList<>();
+        Persona ezequiel = new Persona(41442040, "Ezequiel", "Vargas", fechaHoy);
+        Persona juan = new Persona(20412230, "Juan", "Rodriguez", fechaHoy);
+        Persona nico = new Persona(12345677, "Nicolas", "Alvarez", fechaHoy);
+        cuentas.add(new CuentaBancaria(1, ezequiel, 78000));
+        cuentas.add(new CuentaBancaria(2, juan, 0));
+        cuentas.add(new CuentaBancaria(3, nico, 0));
+        Banco provincia = new Banco("Provincia", localidadBanco, 1, empleados, cuentas);
+        provincia.mostrarResumen();
     }
 }
--- Practico4/Banco.java
+++ Practico4/Banco.java
@@ -4,7 +4,8 @@ public class Banco {
     private String nombre;
     private int nroSucursal;
     private Localidad localidad;
-    private ArrayList<Empleado> empleados;
+    private ArrayList<Empleado> empleados; // usamos genericos
+    private ArrayList cuentasBancarias; // sin genericos
 
     public Banco(String p_nombre, Localidad p_localidad, int p_nroSucursal, Empleado p_empleado) {
         this.setNombre(p_nombre);
@@ -12,6 +13,8 @@ public class Banco {
         this.setNroSucursal(p_nroSucursal);
         this.setEmpleados(new ArrayList<>());
         this.agregarEmpleado(p_empleado);
+        this.setCuenta(new ArrayList<>());
+
     }
 
     public Banco(String p_nombre, Localidad p_localidad, int p_nroSucursal, ArrayList<Empleado> p_empleados) {
@@ -19,13 +22,23 @@ public class Banco {
         this.setLocalidad(p_localidad);
         this.setNroSucursal(p_nroSucursal);
         this.setEmpleados(p_empleados);
+        this.setCuenta(new ArrayList<>());
+    }
+
+    public Banco(String p_nombre, Localidad p_localidad, int p_nroSucursal, ArrayList<Empleado> p_empleados,
+            ArrayList p_cuentas) {
+        this.setNombre(p_nombre);
+        this.setLocalidad(p_localidad);
+        this.setNroSucursal(p_nroSucursal);
+        this.setEmpleados(p_empleados);
+        this.setCuenta(p_cuentas);
     }
 
     public String getNombre() {
         return this.nombre;
     }
 
-    public void setNombre(String p_nombre) {
+    private void setNombre(String p_nombre) {
         this.nombre = p_nombre;
     }
 
@@ -33,7 +46,7 @@ public class Banco {
         return this.nroSucursal;
     }
 
-    public void setNroSucursal(int p_nroSucursal) {
+    private void setNroSucursal(int p_nroSucursal) {
         this.nroSucursal = p_nroSucursal;
     }
 
@@ -41,7 +54,7 @@ public class Banco {
         return this.localidad;
     }
 
-    public void setLocalidad(Localidad p_localidad) {
+    private void setLocalidad(Localidad p_localidad) {
         this.localidad = p_localidad;
     }
 
@@ -49,8 +62,23 @@ public class Banco {
         return this.empleados;
     }
 
-    public void setEmpleados(ArrayList<Empleado> p_empleados) {
+    private void setEmpleados(ArrayList<Empleado> p_empleados) {
         this.empleados = p_empleados;
+    }
+
+    /**
+     * @param p_cuenta
+     *                 recibe una lista y lo setea en el atributo cuentasBancarias
+     */
+    private void setCuenta(ArrayList p_cuenta) {
+        this.cuentasBancarias = p_cuenta;
+    }
+
+    /**
+     * @return devuelve una lista de cuentas
+     */
+    public ArrayList getCuenta() {
+        return this.cuentasBancarias;
     }
 
     public boolean agregarEmpleado(Empleado p_empleado) {
@@ -64,6 +92,59 @@ public class Banco {
         } else {
             return this.getEmpleados().remove(p_empleado);
         }
+    }
+
+    public boolean agregarCuentaBancaria(CuentaBancaria p_cuenta) {
+        return this.getCuenta().add(p_cuenta);
+    }
+
+    public boolean quitarCuentaBancaria(CuentaBancaria p_cuenta) {
+        return this.getCuenta().remove(p_cuenta);
+    }
+
+    private void mostrarSaldoCero() {
+        System.out.println("\nTitulares con Cuenta en Saldo Cero\n");
+        System.out.println("-------------------------------------------------------\n");
+        for (CuentaBancaria cuenta : (ArrayList<CuentaBancaria>) this.getCuenta()) {
+            if (cuenta.getSaldo() == 0) {
+                System.out.printf("%d\t\t%s\n", cuenta.getTitular().getDNI(), cuenta.getTitular().apeYNom());
+            }
+        }
+        System.out.println("-------------------------------------------------------\n");
+    }
+
+    private int cuentasSaldoActivo() {
+        int cuentasActivas = 0;
+        for (CuentaBancaria cuenta : (ArrayList<CuentaBancaria>) this.getCuenta()) {
+            if (cuenta.getSaldo() > 0) {
+                cuentasActivas++;
+            }
+        }
+        return cuentasActivas;
+    }
+
+    public void mostrarResumen() {
+        System.out.printf("Banco: %s\tSucursal: %d\nLocalidad: %s\tProvincia: %s\n\n", this.getNombre(),
+                this.getNroSucursal(), this.getLocalidad().getNombre(), this.getLocalidad().getProvincia());
+        System.out.println("************************************************************\n" +
+                "RESUMEN DE CUENTAS BANCARIAS\n" +
+                "************************************************************\n");
+        System.out.printf(
+                "Número total de Cuentas Bancarias: %d\nCuentas Activas: %d\nCuentas Saldo Cero: %d\n-------------------------------------------------------",
+                this.cuentasSaldoActivo() + this.cuentasSaldoCero(), this.cuentasSaldoActivo(),
+                this.cuentasSaldoCero());
+        this.mostrarSaldoCero();
+
+    }
+
+    private int cuentasSaldoCero() {
+        int cuentasSaldoCero = 0;
+        for (CuentaBancaria cuenta : (ArrayList<CuentaBancaria>) this.getCuenta()) {
+            if (cuenta.getSaldo() == 0) {
+                cuentasSaldoCero++;
+            }
+        }
+        return cuentasSaldoCero;
     }
 
     public void listarSueldos() {
@@ -88,5 +169,6 @@ public class Banco {
         this.listarSueldos();
         System.out.printf("\nTotal a Pagar----------------------------------------$%.2f\n",
                 this.sueldosAPagar());
+        this.mostrarSaldoCero();
     }
 }
