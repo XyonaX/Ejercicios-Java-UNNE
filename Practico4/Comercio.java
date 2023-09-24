53374e39ff2de24715c519b2d306c90a316c14ee
XyonaX <jonatan.vargasportillo@gmail.com>
2023-09-24 20:24:53 -0300

    se agrego documentacion a las ultimas dos clases

--- Practico4/Comercio.java
+++ Practico4/Comercio.java
@@ -1,20 +1,39 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Permite administrar los empleados de un comercio.
+ * 
+ * @author Vargas Portillo,Jonatan Ezequiel.
+ * @author X, Ingrid Noelí.
+ */
+
 public class Comercio {
     private String nombre;
     private HashMap<Long, Empleado> empleados;
 
+    /**
+     * Constructor que inicializa los atributos por parametro.
+     * 
+     * @param p_nombre String;
+     */
     public Comercio(String p_nombre) {
         this.setNombre(p_nombre);
         this.setEmpleados(new HashMap<Long, Empleado>());
     }
 
+    /**
+     * Constructor que inicializa los atributos por parametro.
+     * 
+     * @param p_nombre    String;
+     * @param p_empleados HashMap<Long, Empleado>;
+     */
     public Comercio(String p_nombre, HashMap<Long, Empleado> p_empleados) {
         this.setNombre(p_nombre);
         this.setEmpleados(p_empleados);
     }
 
+    // Accessors
     public String getNombre() {
         return this.nombre;
     }
@@ -31,32 +50,68 @@ public class Comercio {
         this.empleados = p_empleados;
     }
 
+    // Fin Accessors
+
+    /**
+     * Metodo para agregar Empleados
+     * 
+     * @param p_empleado Empleado;
+     */
     public void altaEmpleado(Empleado p_empleado) {
         this.getEmpleados().put(p_empleado.getCuil(), p_empleado);
     }
 
+    /**
+     * Metodo para quitar Empleados y Retorna el empleado eliminado
+     * 
+     * @param p_cuild long;
+     * @return Empleado;
+     */
     public Empleado bajaEmpleado(long p_cuil) {
         return this.getEmpleados().remove(p_cuil);
     }
 
+    /**
+     * Metodo que retorna la cantidad de empleados existentes
+     */
     public int cantidadDeEmpleados() {
         return this.getEmpleados().size();
     }
 
+    /**
+     * Metodo para saber si es empleado del comercio
+     * 
+     * @param p_cuil long;
+     * @return boolean;
+     */
     public boolean esEmpleado(long p_cuil) {
         return this.getEmpleados().containsKey(p_cuil);
     }
 
+    /**
+     * Metodo para buscar un empleado y lo retorna
+     * 
+     * @param p_cuil long;
+     * @return Empleado;
+     */
     public Empleado buscarEmpleado(long p_cuil) {
         return this.getEmpleados().get(p_cuil);
     }
 
+    /**
+     * Metodo para saber el sueldo neto de un empleado
+     * 
+     * @param p_cuil long;
+     */
     public void sueldoNeto(long p_cuil) {
         Empleado empleadoBuscado = this.buscarEmpleado(p_cuil);
         System.out.printf("Sueldo neto del empleado %s: %.2f\n", empleadoBuscado.nomYApe(),
                 empleadoBuscado.sueldoNeto());
     }
 
+    /**
+     * Metodo que muestra la nomina de empleados
+     */
     public void nomina() {
         System.out.printf("**** Nomina de empleados de %s **** \n", this.getNombre());
         for (Map.Entry<Long, Empleado> empleado : this.getEmpleados().entrySet()) {
--- Practico4/Curso.java
+++ Practico4/Curso.java
@@ -1,20 +1,40 @@
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Permite administrar Cursos con sus alumnos
+ * 
+ * @author X, Ingrid Noelí.
+ * @author Vargas Portillo,Jonatan Ezequiel.
+ */
+
 public class Curso {
     private String nombre;
     private HashMap<Integer, Alumno> alumnos;
 
+    /**
+     * Constructor que inicializa los atributos por parametro.
+     * Y inicializa una coleccion con el HashMap
+     * 
+     * @param p_nombre String;
+     */
     public Curso(String p_nombre) {
         this.setNombre(p_nombre);
         this.setAlumnos(new HashMap<Integer, Alumno>());
     }
 
+    /**
+     * Constructor que inicializa los atributos por parametro.
+     * 
+     * @param p_nombre String;
+     * @param p_alumno HashMap<Integer, Alumno>;
+     */
     public Curso(String p_nombre, HashMap<Integer, Alumno> p_alumnos) {
         this.setNombre(p_nombre);
         this.setAlumnos(p_alumnos);
     }
 
+    // Accessors
     public String getNombre() {
         return this.nombre;
     }
@@ -31,41 +51,72 @@ public class Curso {
         this.alumnos = p_alumnos;
     }
 
+    // Fin Accessors
+
+    /**
+     * Agrega alumno recibido por parametro.
+     * 
+     * @param p_alumno Alumno;
+     */
     public void inscribirAlumno(Alumno p_alumno) {
         this.getAlumnos().put(p_alumno.getLu(), p_alumno);
     }
+
+    /**
+     * Quita al alumno recibido por parametro.
+     * 
+     * @param p_empleados Empleado;
+     * @return el alumno quitado de la coleccion;
+     */
 
     public Alumno quitarAlumno(int p_lu) {
         return this.getAlumnos().remove(p_lu);
     }
 
+    /**
+     * Metodo que retorna la cantidad de alumnos inscriptos
+     * 
+     * @return int; la cantidad de alumnos
+     */
     public int cantidadDeAlumnos() {
         return this.getAlumnos().size();
     }
 
-    // public boolean estaInscripto(int p_lu) {
-    // boolean flag = false;
-
-    // for (Map.Entry<Integer, Alumno> e : this.getAlumnos().entrySet()) {
-    // if (e.getKey() == p_lu) {
-    // flag = true;
-    // }
-    // }
-    // return flag;
-    // }
-
+    /**
+     * Metodo para saber si un alumno esta inscripto
+     * 
+     * @param p_lu int;
+     * @return boolean; retorna true o false
+     */
     public boolean estaInscripto(int p_lu) {
         return this.getAlumnos().containsKey(p_lu);
     }
 
+    /**
+     * Metodo para saber si un alumno esta inscripto
+     * 
+     * @param p_alumno Alumno;
+     * @return boolean; retorna true o false
+     */
     public boolean estaInscripto(Alumno p_alumno) {
         return this.getAlumnos().containsValue(p_alumno);
     }
 
+    /**
+     * Metodo para buscar un alumno
+     * 
+     * @param p_lu int;
+     * @return Alumno; retorna un alumno
+     */
     public Alumno buscarAlumno(int p_lu) {
         return this.getAlumnos().get(p_lu);
     }
 
+    /**
+     * Metodo que imprime el promedio del alumno buscado
+     * 
+     * @param p_lu int;
+     */
     public void imprimirPromedioDelAlumno(int p_lu) {
         if (this.estaInscripto(p_lu)) {
             Alumno alumno = buscarAlumno(p_lu);
@@ -76,6 +127,10 @@ public class Curso {
         }
     }
 
+    /**
+     * Metodo que muestra informacion sobre los alumnos inscriptos
+     * en el curso
+     */
     public void mostrarInscriptos() {
         System.out.printf("****-- Cantidad de inscriptos: %d\n", this.cantidadDeAlumnos());
         for (Map.Entry<Integer, Alumno> alumno : this.getAlumnos().entrySet()) {
